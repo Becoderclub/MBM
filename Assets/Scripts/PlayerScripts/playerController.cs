31734c9d3a854e0e028528b2cfcd98e4a451182f
Artanis28 <raisky.ivan@gmail.com>
2020-04-23 18:03:55 +0300

    tilemove

--- Assets/Scripts/PlayerScripts/playerController.cs
+++ Assets/Scripts/PlayerScripts/playerController.cs
@@ -7,28 +7,30 @@ public class playerController : MonoBehaviour
     private float _speed;
     private Animator _animator;
     private Vector2 _movement;
-    private Rigidbody2D _rigidbody;
+    private Vector2 _destination;
     void Start() 
     {
        _speed = 5f;
        _animator = GetComponent<Animator>();
-       _rigidbody = GetComponent<Rigidbody2D>();
+       _destination = transform.position;
     }
     void Update()
     {
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
 
+        _destination = transform.position + (Vector3)_movement;
+
         AnimateMovement();
     }
     private void FixedUpdate() 
     {
-        _rigidbody.MovePosition(_rigidbody.position + _movement * _speed * Time.fixedDeltaTime);    
+        transform.position = Vector3.MoveTowards(transform.position, _destination, _speed * Time.fixedDeltaTime);    
     }
     public void AnimateMovement()
     {
-         _animator.SetFloat("x", _movement.x);
-         _animator.SetFloat("y", _movement.y);
-         _animator.SetFloat("Speed", _movement.sqrMagnitude);
+        _animator.SetFloat("x", _movement.x);
+        _animator.SetFloat("y", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
     }
 }
