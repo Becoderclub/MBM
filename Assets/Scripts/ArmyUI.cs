bd6664e98655d2de48b847598a0d7da5af3922bc
Doodlinka <oleshko.darya@gmail.com>
2020-05-12 19:37:04 +0300

    inventory scripts test 1

---
+++ Assets/Scripts/ArmyUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmyUI : MonoBehaviour
+{
+    [SerializeField] private Slot[] slots = new Slot[PlayerArmy.armySize];
+
+    void Start() {
+        PlayerArmy.Init();
+        Refresh();
+    }
+
+    public void Refresh() {
+        for (int i = 0; i < PlayerArmy.armySize; i++) {
+            slots[i].contentName.text = PlayerArmy.army[i].ClassName;
+            // Warrior has no sprite field
+            //slots[i].image.sprite = army[i].sprite;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerArmy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerArmy
+{
+    public const int armySize = 6;
+    public static Warrior[] army;
+    public static Warrior empty = ScriptableObject.CreateInstance<Warrior>();
+
+    public static void Init() {
+        // load army here, empty arr for now
+        army = new Warrior[armySize];
+        for (int i = 0; i < armySize; i++) {
+            if (army[i] == null) {
+                army[i] = empty;
+            }
+        }
+        army[0] = ScriptableObject.CreateInstance<Elf>();
+    }
+}
