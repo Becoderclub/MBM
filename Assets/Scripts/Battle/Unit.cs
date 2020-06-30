f7aa17a54a46a5610eeca6f75e72ba2d82cc7c05
Doodlinka <oleshko.darya@gmail.com>
2020-06-30 15:31:23 +0300

    unit monobehaviour added, with method Load(Warrior, amount, isEnemy)

---
+++ Assets/Scripts/Battle/Unit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    int maxhp, attack, defense, magicdef, turns;
+    bool canShoot;
+    string unitName;
+
+    int amount = 1;
+    int health;
+    bool isEnemy;
+    //int healthOfUnit;
+
+    void Load(Warrior w, int amount, bool isEnemy) {
+        maxhp = w.hp;
+        attack = w.attackMight;
+        defense = w.armor;
+        magicdef = w.magicResistance;
+        turns = w.turnsAmount;
+        canShoot = w.isAbleToShoot;
+        unitName = w.className;
+
+        this.amount = amount;
+        this.isEnemy = isEnemy;
+        health = maxhp * amount;
+        //healthOfUnit = maxhp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Battle/Warrior.cs
+++ Assets/Scripts/Battle/Warrior.cs
@@ -6,29 +6,14 @@ using UnityEngine;
 public class Warrior : ScriptableObject
 {
     //hp & attackMight is for every single unit of amount, so i must multiply it somewhere
-    [SerializeField]protected int amount;
     
-    [SerializeField]protected int hp;
-    [SerializeField]protected bool isEnemy;
-    [SerializeField]protected int magicResistance;
-    [SerializeField]protected int turnsAmount;
-    [SerializeField]protected bool isAbleToShoot;
-    [SerializeField]protected int attackMight;
-    [SerializeField]protected int armor;
-    [SerializeField]protected int worth;
+    [SerializeField]public int hp {get;}
+    [SerializeField]public int magicResistance{get;}
+    [SerializeField]public int turnsAmount{get;}
+    [SerializeField]public bool isAbleToShoot{get;}
+    [SerializeField]public int attackMight{get;}
+    [SerializeField]public int armor{get;}
+    [SerializeField]public int worth{get;}
     
-    [SerializeField]protected string className;
-    public string ClassName
-    {
-        get => className;
-    }
-    public int Amount
-    {
-        get => amount;
-        set => amount = value;
-    }
-    public int Worth
-    {
-        get => worth;
-    }
+    [SerializeField]public string className{get;}
 }
